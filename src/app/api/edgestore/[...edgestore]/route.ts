4818a3a4787b2a6104e9abb2932eb21387349f38
mohe22 <yln0501403845@gmail.com>
2024-09-02 10:48:41 +0800

    upload images to edge store

--- src/app/api/edgestore/[...edgestore]/route.ts
+++ src/app/api/edgestore/[...edgestore]/route.ts
@@ -3,9 +3,7 @@ import { createEdgeStoreNextHandler } from '@edgestore/server/adapters/next/app'
 
 const es = initEdgeStore.create();
 
-/**
- * This is the main router for the Edge Store buckets. And am a bitch`
- */
+
 const edgeStoreRouter = es.router({
   publicFiles: es.fileBucket(),
 });
@@ -16,7 +14,5 @@ const handler = createEdgeStoreNextHandler({
 
 export { handler as GET, handler as POST };
 
-/**
- * This type is used to create the type-safe client for the frontend.
- */
+
 export type EdgeStoreRouter = typeof edgeStoreRouter;
